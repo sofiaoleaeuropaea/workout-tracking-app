293a2ae23a6d01d8eb87e2ba62c3837c09c59e22
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-10-08 18:37:48 +0100

    Create hero content and style

---
+++ views/home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ironclad</title>
+    <link href="https://api.fontshare.com/v2/css?f[]=general-sans@300,400,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+
+</head>
+
+<body>
+    <?php require("templates/navbar.php"); ?>
+    <main>
+        <div class="container">
+            <div class="hero__wrapper">
+                <div class="hero__content__background">
+                    <h2>Start counting now.</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <a class="btn" href="<?= ROOT ?>/register">Start >></a>
+                </div>
+                <div>
+                    <h1>Take control of your training</h1>
+                    <p>Visualize your progress with the Ironclad app. Keep track of your best sets, body fat percentage, and more.</p>
+                    <a href="">Read more</a>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
