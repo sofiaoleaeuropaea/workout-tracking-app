4dea3ccf47f27fcc7e22e119341a02c59b93b371
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-10-18 19:07:30 +0100

    Create navbar for gymtracker dashboard

--- views/templates/navbar.php
+++ views/templates/navbar.php
@@ -4,16 +4,51 @@
             <a href="<?= ROOT ?>/"><img src="/images/ironclad_logo.png" class="img-fluid logo" alt="Ironclad Logo" /></a>
             <nav>
                 <ul class="navbar__menu">
-                    <li class="btn"><a href="<?= ROOT ?>/">About us</a></li>
-                    <li class="btn"><a href="<?= ROOT ?>/">Contacts</a></li>
+
                     <?php
-                    if (isset($_SESSION["user_id"])) {
+                    if (!isset($_SESSION["user_id"])) {
                     ?>
-                        <li class="btn"><a href="<?= ROOT ?>/logout/">Logout</a></li>
+                        <li class="btn"><a href="<?= ROOT ?>/">About us</a></li>
+                        <li class="btn"><a href="<?= ROOT ?>/">Contacts</a></li>
+                        <li class="btn"><a href="<?= ROOT ?>/gymtracker/login">Login</a></li>
                     <?php
                     } else {
                     ?>
-                        <li class="btn"><a href="<?= ROOT ?>/login/">Login</a></li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/gymtracker/dashboard/">
+                                <i class="material-icons">dashboard</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/gymtracker/workout-plans/">
+                                <i class="material-icons">fitness_center</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/gymtracker/tracker/">
+                                <i class="material-icons">track_changes</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/gymtracker/calendar/">
+                                <i class="material-icons">calendar_today</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/gymtracker/biometrics/">
+                                <i class="material-icons">insights</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/gymtracker/userprofile/">
+                                <i class="material-icons">account_circle</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/gymtracker/logout/">
+                                <i class="material-icons">logout</i>
+                            </a>
+                        </li>
                     <?php
                     }
                     ?>
