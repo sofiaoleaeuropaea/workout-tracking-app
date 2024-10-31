94428200753b3f845f001214606d0b8994e9c249
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-10-31 01:33:25 +0000

    Create about e contacs content

---
+++ views/about.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ironclad</title>
+    <link href="https://api.fontshare.com/v2/css?f[]=general-sans@300,400,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+
+</head>
+
+<body>
+    <?php require("templates/navbar.php"); ?>
+    <main>
+        <div class="container">
+            <section>
+                <div class="">
+                    <h2>What Ironclad is about</h2>
+                    <p>Welcome to your ultimate workout companion! The Ironclad app is designed to help you stay motivated, organized, and accountable on your fitness journey. Whether you’re a beginner or a seasoned athlete, our app provides the tools you need to reach your fitness goals.</p>
+
+                    <a class="btn" href="<?= ROOT ?>/register">Start >></a>
+                </div>
+            </section>
+            <section>
+                <h3>Our features</h3>
+                <div class="form_container">
+                    <h4>Workout Tracker</h4>
+                    <p class="card-text">Easily record your workouts, track exercises, sets, reps, and weights for optimal performance analysis.</p>
+                </div>
+                <div class="form_container">
+                    <h4>Custom Workouts</h4>
+                    <p>Create your personalized workouts tailored to your fitness level and goals, making each session effective and enjoyable.</p>
+                </div>
+                <div class="form_container">
+                    <h4>Training Schedule</h4>
+                    <p class="card-text">Plan and schedule your workouts to ensure consistency and stay on track with your fitness regimen.</p>
+                </div>
+            </section>
+        </div>
+    </main>
+
+</body>
+
+</html>
---
+++ views/contacts.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ironclad</title>
+    <link href="https://api.fontshare.com/v2/css?f[]=general-sans@300,400,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+
+</head>
+
+<body>
+    <?php require("templates/navbar.php"); ?>
+    <main>
+        <div class="container">
+            <div className="contact__information__wrapper ">
+                <div className="contact__information__details">
+                    <div>
+                        <h2>We would love to hear from you!</h2>
+                        <p>If you have any questions or feedback, please reach out to us.</p>
+                    </div>
+                    <div class="form_container form_container-w">
+                        <?php
+                        if (isset($message)) {
+                            echo '<p role="alert">' . $message . '</p>';
+                        }
+                        ?>
+                        <form method="POST" action="<?= ROOT ?>/contacts/" enctype="multipart/form-data">
+                            <div class="input_container">
+                                <label class="input_label" for="input_name">Name</label>
+                                <input class="input_field" id="input_name" type="text" name="name" required minlength="3" maxlength="100">
+                            </div>
+                            <div class="input_container">
+                                <label class="input_label" for="input_email">Email</label>
+                                <input id="input_email" class="input_field" type="email" name="email" required>
+                            </div>
+                            <div class="input_container">
+                                <label class="input_label" for="plan_description">Message</label>
+                                <textarea class="input_field" name="form_message" rows="4" cols="50"></textarea>
+                            </div>
+                            <div>
+                                <button type="submit" name="register" class="btn">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <address>
+                            <a>Edifício Mirage, R. Dr. Eduardo Neves, Nº3, 1050-077 Lisboa</a>
+                        </address>
+
+                        <address>
+                            <a href="mailto:support@ironclad.pt">support@ironclad.pt</a>
+                        </address>
+
+                        <address>
+                            <a href="tel:+351239000000">(+351) 239000000</a>
+                        </address>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
