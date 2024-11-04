582876416a995becea0a7d589c6909a37f752b83
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-11-04 04:08:29 +0000

    Add admin routing to home controller

--- controllers/home.php
+++ controllers/home.php
@@ -3,4 +3,8 @@ if (isset($_SESSION["user_id"])) {
     header("Location: " . ROOT . "/gymtracker/dashboard/");
     exit();
 }
+if (isset($_SESSION["admin_id"])) {
+    header("Location: " . ROOT . "/admin/dashboard/");
+    exit();
+}
 require("views/home.php");
