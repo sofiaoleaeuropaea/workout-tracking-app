c9838ff6afb8f46ed7391f6f034195ec7fec1e18
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-11-04 03:28:16 +0000

    Add navbar for admin area

---
+++ views/templates/navbar-admin.php
@@ -0,0 +1,38 @@
+<div class="navbar">
+    <div class="container">
+        <div class="navbar__wrapper">
+            <a href="<?= ROOT ?>/"><img src="/images/ironclad_logo.png" class="img-fluid logo" alt="Ironclad Logo" /></a>
+            <nav>
+                <nav>
+                    <ul class="navbar__menu">
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/admin/dashboard/">
+                                <i class="material-icons">space_dashboard</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/admin/users-management/">
+                                <i class="material-icons">manage_accounts</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/admin/workout-library/">
+                                <i class="material-icons">assignment</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/admin/support/">
+                                <i class="material-icons">support</i>
+                            </a>
+                        </li>
+                        <li class="btn">
+                            <a href="<?= ROOT ?>/admin/logout/">
+                                <i class="material-icons">logout</i>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </nav>
+        </div>
+    </div>
+</div>
