ae365f0d74e7871c7a1bd061a6b8cc87199ed5ba
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-11-04 04:09:08 +0000

    Change default picture path

--- controllers/gymtracker/dashboard.php
+++ controllers/gymtracker/dashboard.php
@@ -5,12 +5,11 @@ require("models/trainingschedule.php");
 $modelUsers = new Users();
 $modelTrainingSchedule = new TrainingSchedule();
 
-// TODO: esspecificar o metodo get??
 if (isset($_SESSION["user_id"])) {
 
     $user = $modelUsers->getUserById($_SESSION["user_id"]);
     if ($user) {
-        $defaultPhoto = '/public/images/default-profile-icon.webp';
+        $defaultPhoto = '/images/userprofile/default-profile-icon.webp';
         $user['photo'] = !empty($user['photo']) ? '/' . ltrim($user['photo'], '/') : $defaultPhoto;
 
         $exercises = $modelTrainingSchedule->getTrainingScheduleByCurrDate($_SESSION["user_id"]);
